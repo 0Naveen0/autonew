9368573e2a167f914f1332ee3d443d8de638b462
0Naveen0 <naveenkamal.2011@gmail.com>
2024-07-02 18:33:01 +0530

    Authentication Added

--- portfolio_v1.1/portfolio/urls.py
+++ portfolio_v1.1/portfolio/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', include("pages.urls")),
     
     path('admin/', admin.site.urls),
+    path('accounts/',include("accounts.urls")),
+    path('accounts/',include("django.contrib.auth.urls")),
    
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT )
